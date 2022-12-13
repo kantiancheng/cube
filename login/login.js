4ca802058f28a7028ef156f7fb749640f57eea5b
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-12-12 17:27:21 -0800

    Save

--- login/login.js
+++ login/login.js
@@ -9,6 +9,7 @@ function login() {
         if (User_Library[i][0].toLowerCase() == Username.toLowerCase()) {
             if (Password == Password[i][1]) {
                 sessionStorage.setItem("User_Name", User_Library[i][0]);
+                window.location.reload();
             } else { alert("wrong password"); }
         } else {
             alert("username error");
