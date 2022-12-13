607916cad5be61f720605f31ca4f19a44c205508
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-12-12 17:33:54 -0800

    Save

--- assets/js/login_check.js
+++ assets/js/login_check.js
@@ -1,6 +1,6 @@
 let User_Name = sessionStorage.getItem("User_Name");
 
-if (Account_login_status == "" || Account_login_status == null) {
+if (User_Name == "" || User_Name == null) {
     //未登录
     window.location = "./login/";
 } else if (User_Name != "" && User_Name != null) {
