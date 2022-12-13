0d38138d3266899f2019c8dabb7f891f010aaff3
Tony Kan <95247738+kantiancheng@users.noreply.github.com>
2022-12-12 17:46:07 -0800

    Save

--- login/login.js
+++ login/login.js
@@ -7,11 +7,15 @@ function login() {
     let Password = document.getElementById("Password").value;
     for (let i = 0; i < User_Library.length; i++) {
         if (User_Library[i][0].toLowerCase() == Username.toLowerCase()) {
+            console.log(User_Library[i][0].toLowerCase() + "==" + Username.toLowerCase());
+            console.log("用户名正确！");
             if (Password == Password[i][1]) {
+                console.log(User_Library[i][1].toLowerCase() + "==" + Password.toLowerCase());
+                console.log("密码正确！");
                 sessionStorage.setItem("User_Name", User_Library[i][0]);
                 window.location.reload();
             } else if (i == (User_Library.length - 1)) { alert("wrong password"); }
-        } else if (i == (User_Library.length - 1)){
+        } else if (i == (User_Library.length - 1)) {
             alert("username error");
         }
     }
